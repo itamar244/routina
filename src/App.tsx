400067378ca53ee164a4c88e38f07f2646da4ea7
Itamar Yatom <itamary24@gmail.com>
2020-09-26 21:15:27 +0300

    add a junkty idea

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { DragEventHandler, MouseEventHandler, ReactNode, useCallback, useState } from "react";
+import "./App.css";
 
-function App() {
+function DraggableButton({ children }: { children?: ReactNode }) {
+  const [position, setPosition] = useState<{
+    x?: number;
+    y?: number;
+  }>({});
+  const [isDragging, setIsDragging] = useState(false);
+  const onDrag: DragEventHandler = useCallback(
+    (event) => {
+      event.preventDefault();
+      setPosition({ x: event.clientX, y: event.clientY });
+      setIsDragging(event.clientX !== 0 || event.clientY !== 0);
+    },
+    [setPosition, setIsDragging]
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button
+      className="drag-option"
+      draggable
+      onDrag={onDrag}
+      style={
+        isDragging
+          ? {
+              position: "absolute",
+              transform: "translate(-50%, -50%)",
+              left: position.x,
+              top: position.y,
+            }
+          : {}
+      }
+    >
+      {children}
+    </button>
   );
 }
 
-export default App;
+function DraggableOnHoverButton({ children }: { children?: ReactNode }) {
+  const [position, setPosition] = useState<{
+    x?: number;
+    y?: number;
+  }>({});
+  const [isDragging, setIsDragging] = useState(false);
+  const onDrag: MouseEventHandler = useCallback(
+    (event) => {
+      console.log(event.clientX);
+      event.preventDefault();
+      setPosition({ x: event.clientX, y: event.clientY });
+      setIsDragging(event.clientX !== 0 || event.clientY !== 0);
+    },
+    [setPosition, setIsDragging]
+  );
+  return (
+    <button
+      className="drag-option"
+      draggable
+      onDrag={onDrag}
+      style={
+        isDragging
+          ? {
+              position: "absolute",
+              transform: "translate(-50%, -50%)",
+              left: position.x,
+              top: position.y,
+            }
+          : {}
+      }
+    >
+      {children}
+    </button>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Routina</header>
+      <p>asdfasdf</p>
+      <div>
+        <DraggableButton>meals</DraggableButton>
+        <DraggableButton>pisses</DraggableButton>
+      </div>
+      <div>
+        <DraggableOnHoverButton>3</DraggableOnHoverButton>
+      </div>
+    </div>
+  );
+}
